a7ac2902ea4adeb00000f9f8eaaf6e0f05f0c616
Andrew Hooker <geekoncoffee@gmail.com>
2014-05-07 07:55:28 -0500

    Formatting improvements

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,16 @@ class SearchController < ApplicationController
   include RottenTomatoes
 
   def lookup
-
     Rotten.api_key = RT_KEY
     movies = RottenMovie.find(:title => params[:q], :limit => 50)
-    movie_collection = movies.collect{|movie| {title: CGI.escapeHTML(movie.title), description: CGI.escapeHTML(movie.synopsis), image: movie.posters.thumbnail.html_safe, year: movie.year}}
+    movie_collection = movies.collect{|movie| 
+                        {
+                          title: CGI.escapeHTML(movie.title), 
+                          description: CGI.escapeHTML(movie.synopsis), 
+                          image: movie.posters.thumbnail.html_safe, 
+                          year: movie.year
+                        }
+                      }
     render :json => {:status => "200"}.merge(options: movie_collection)
   end
 end
