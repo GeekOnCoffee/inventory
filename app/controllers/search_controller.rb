c61cf8d00b8f4ba8e0788f2504a0d5ac403d1b22
Andrew Hooker <geekoncoffee@gmail.com>
2014-05-06 23:02:17 -0500

    Style tweaks

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
   def lookup
 
     Rotten.api_key = RT_KEY
-    movies = RottenMovie.find(:title => params[:q], :limit => 10)
+    movies = RottenMovie.find(:title => params[:q], :limit => 50)
     movie_collection = movies.collect{|movie| {title: CGI.escapeHTML(movie.title), description: CGI.escapeHTML(movie.synopsis), image: movie.posters.thumbnail.html_safe, year: movie.year}}
     render :json => {:status => "200"}.merge(options: movie_collection)
   end
